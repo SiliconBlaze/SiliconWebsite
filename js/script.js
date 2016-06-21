84ba5f181abf641c39821c7582f3874091a1fbc9
Piusdan <npiusdan@gmail.com>
2016-06-21 18:19:11 +0300

    Iitial commit

--- js/script.js
+++
@@ -1,9 +0,0 @@
-/*var gmapAPIkey = "AIzaSyCfO-kSMHpWJEXatrZO1MeMAIFIXjlhRlA";
-
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-      }
